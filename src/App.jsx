b8ec65329746ec8c4a2fd9de1f67f803733e9529
ibh2511 <isabelle.haugan@gmail.com>
2025-11-15 00:07:46 +0100

    chore: configure GitHub Pages deployment with gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LandingPage from "./pages/LandingPage"
 import AdminPage from "./pages/AdminPage"
 import RegisterPage from "./pages/RegisterPage"
-import RegisterPage from "./pages/StoresPage"
+import StoresPage from "./pages/StoresPage"
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/registrer" element={<RegisterPage />} />
+        <Route path="/butikker" element={<StoresPage />} />
       </Routes>
     </BrowserRouter>
   )
