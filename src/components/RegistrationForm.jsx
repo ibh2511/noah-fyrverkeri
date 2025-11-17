09da2bbd7ef61adf6f594228f611c497eb9621fc
ibh2511 <isabelle.haugan@gmail.com>
2025-11-17 17:40:24 +0100

    feat: new GAS link

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -63,8 +63,7 @@ function setupVisitorTracking() {
 
 // TODO: sett inn din ekte Apps Script URL her
 const GAS_URL =
-  "https://script.google.com/macros/s/AKfycbybo8i1sRgh1hBQiVoc2ZXDS-UeI7-7ZN-YjNNyNrnCEAPT46orrf02uvyBKBwskomusQ/exec"
-// Lokale konstanter for lodd
+  "https://script.google.com/macros/s/AKfycbwAlE6c1IepopAI152oeW4-fRWKBUHfrLS9mTruiERfcab-ii5UrTWpNT4aF728086zqA/exec" // Lokale konstanter for lodd
 const LOTTERY_ACTIONS = [
   {
     id: "MailAlle",
